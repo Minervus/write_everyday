dbfa92416c05bc65cd027e1556a0ca413d27f18d
Tony Nguyen <tnguyen411@gmail.com>
2024-10-01 19:43:18 -0700

    foundation pieces

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"; 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express(); 
+const port = 3000; 
+
+app.listen(port, () => {
+    console.log(`Server listenting on port ${port}`); 
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
